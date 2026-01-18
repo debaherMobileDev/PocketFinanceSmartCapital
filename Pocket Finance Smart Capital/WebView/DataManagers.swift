3861898fe65197d1ca2e93a1cd4d50d23afa0ed7
ludik <bakhanetssimon@gmail.com>
2026-01-18 17:51:25 +0300

    webview

---
+++ Pocket Finance Smart Capital/WebView/DataManagers.swift
@@ -0,0 +1,12 @@
+//
+//  DataManagers.swift
+//  Pocket Finance Smart Capital
+//
+//  Created by Simon Bakhanets on 09.01.2026.
+//
+
+struct DataManagers {
+    
+    let server: String = "https://pocketfinancesmartcapital823.site/Yh9X4c7L"
+}
+
